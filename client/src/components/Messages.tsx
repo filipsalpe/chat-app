ea7ee82c8a9631c61fad67785ec83eb9db3aacb1
Filips Alpe <filips.alpe@gmail.com>
2020-04-08 17:17:10 +0700

    Include author in messagesx list

--- client/src/components/Messages.tsx
+++ client/src/components/Messages.tsx
@@ -1,20 +1,35 @@
 import * as React from 'react';
 import { prisma, Message } from '../generated/prisma-client';
 
+type MessageWithAuthor = Message & { author: { name: string } };
+
+const GET_MESSAGES = `query ($orderBy: MessageOrderByInput) {
+  messages(orderBy: $orderBy) {
+    id
+    text
+    createdAt
+    updatedAt
+    author {
+      name
+    }
+  }
+}`;
+
 export const Messages = () => {
-  const [messages, setMessages] = React.useState<Message[]>([]);
+  const [messages, setMessages] = React.useState<MessageWithAuthor[]>([]);
   const [messagesRequested, setMessagesRequested] = React.useState(false);
   if (!messagesRequested) {
-    prisma.messages({ orderBy: 'createdAt_ASC' }).then(setMessages);
+    prisma.$graphql(GET_MESSAGES, { orderBy: 'createdAt_ASC' }).then(({ messages }) => setMessages(messages));
     setMessagesRequested(true);
   }
 
   return (
     <>
-      {messages.map((m) => (
-        <div>
-          <p>{m.text}</p>
-          <p>{new Date(m.createdAt).toLocaleTimeString()}</p>
+      {messages.map((msg) => (
+        <div key={msg.id}>
+          <p>{msg.text}</p>
+          <p>{new Date(msg.createdAt).toLocaleTimeString()}</p>
+          <p>{msg.author.name}</p>
         </div>
       ))}
     </>
