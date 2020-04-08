b0d65d183007b227b52ca43b5cc16ce41e9ef031
Filips Alpe <filips.alpe@gmail.com>
2020-04-08 18:35:40 +0700

    Remove UserInfo component
    It isn't really required

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { UserProvider } from './authentication/UserContext';
 import { Layout } from './layout/Layout';
 import { Tabs, Tab } from './layout/Tabs';
 import { RoomTitle } from './components/RoomTitle';
-import { UserInfo } from './components/UserInfo';
 import { UsersList } from './components/UsersList';
 import { Messages } from './components/Messages';
 import { MessageInput } from './components/MessageInput';
@@ -18,7 +17,6 @@ function App() {
         <Tabs activeTab={activeTab} onRequestChange={setActiveTab} />
         {activeTab === Tab.CHAT ? <Messages /> : <UsersList />}
         <MessageInput />
-        <UserInfo />
       </Layout>
     </UserProvider>
   );
