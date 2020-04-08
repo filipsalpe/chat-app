771bd4bb8eeb40393ff495c74acc40429e7539ec
Filips Alpe <filips.alpe@gmail.com>
2020-04-08 18:42:48 +0700

    Style the participants list

--- client/src/components/UsersList.tsx
+++ client/src/components/UsersList.tsx
@@ -1,5 +1,17 @@
 import * as React from 'react';
 import { prisma, User } from '../generated/prisma-client';
+import styled from '@emotion/styled/macro';
+
+const List = styled.ul`
+  list-style: none;
+  mergin: 0;
+  padding: 0;
+`;
+const Item = styled.li`
+  line-height: 3em;
+  padding-left: 20px;
+  border-bottom: 1px solid #ebebeb;
+`;
 
 export const UsersList = () => {
   const [users, setUsers] = React.useState<User[]>([]);
@@ -10,10 +22,10 @@ export const UsersList = () => {
   }
 
   return (
-    <ul>
+    <List>
       {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
+        <Item key={user.id}>{user.name}</Item>
       ))}
-    </ul>
+    </List>
   );
 };
